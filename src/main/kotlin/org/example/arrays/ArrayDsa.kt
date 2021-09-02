31032f7addb8d596fc11f62b1ade3cbfad0c5f8a
Paul Adams <ghusiyy@gmail.com>
2021-09-01 20:53:08 -0700

    impl and test for twoSum

--- src/main/kotlin/org/example/arrays/ArrayDsa.kt
+++ src/main/kotlin/org/example/arrays/ArrayDsa.kt
@@ -1,12 +1,12 @@
 package org.example.arrays
 
 fun twoSum(numbs: List<Int>, target: Int): Boolean {
-    val seen = mutableMapOf<Int, Int>()
+    val sums = mutableMapOf<Int, Int>()
     for(n in numbs.iterator()) {
-        if(seen.containsKey(target - n)) {
+        if(sums.containsKey(target - n)) {
             return true
         }
-        seen[n] = 0
+        sums[n] = 0
     }
     return false;
 }
