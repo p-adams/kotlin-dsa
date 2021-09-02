762be15e74f21426d121954c736d76db06d2f60a
Paul Adams <ghusiyy@gmail.com>
2021-09-01 21:05:19 -0700

    comment runtime and space complexity and make test list a class member

--- src/main/kotlin/org/example/arrays/ArrayDsa.kt
+++ src/main/kotlin/org/example/arrays/ArrayDsa.kt
@@ -1,5 +1,6 @@
 package org.example.arrays
 
+// hash table solution: O(n) runtime, O(n) space
 fun twoSum(numbs: List<Int>, target: Int): Boolean {
     val sums = mutableMapOf<Int, Int>()
     for(n in numbs.iterator()) {
--- src/test/kotlin/org/example/arrays/ArrayDsaKtTest.kt
+++ src/test/kotlin/org/example/arrays/ArrayDsaKtTest.kt
@@ -3,15 +3,9 @@ package org.example.arrays
 import junit.framework.TestCase
 
 class ArrayDsaKtTest : TestCase() {
-
-    public override fun setUp() {
-        super.setUp()
-    }
-
-    public override fun tearDown() {}
+    private val numbs1 = listOf<Int>(1, 2, 3, 5, 5)
 
     fun testTwoSum() {
-        val numbs = listOf(1, 2, 3, 5, 5)
-        assertEquals(true, twoSum(numbs, 5))
+        assertEquals(true, twoSum(numbs1, 5))
     }
 }
