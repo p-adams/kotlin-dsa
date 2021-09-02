03061c28fbc7866fa76fced84e0a2ad32418861f
Paul Adams <ghusiyy@gmail.com>
2021-09-01 21:08:22 -0700

    add function description and definition for sorted two sum

--- src/main/kotlin/org/example/arrays/ArrayDsa.kt
+++ src/main/kotlin/org/example/arrays/ArrayDsa.kt
@@ -1,6 +1,8 @@
 package org.example.arrays
 
 // hash table solution: O(n) runtime, O(n) space
+// takes an unsorted list and returns true if any two numbers sum equal
+// target; false, otherwise.
 fun twoSum(numbs: List<Int>, target: Int): Boolean {
     val sums = mutableMapOf<Int, Int>()
     for(n in numbs.iterator()) {
@@ -11,3 +13,5 @@ fun twoSum(numbs: List<Int>, target: Int): Boolean {
     }
     return false;
 }
+// sorted list solution: O(n) runtime, O(1) space
+fun sortedTwoSum(sortedNumbs: List<Int>, target: Int) {}
